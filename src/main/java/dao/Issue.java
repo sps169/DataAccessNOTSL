4a66bff77acbd2d9c4bc59aec5069ae8c81052b4
Sergio Perez <sps169@outlook.es>
2022-01-27 20:40:22 +0100

    DAOs finalizados + lombok en pom

--- src/main/java/dao/Issue.java
+++ src/main/java/dao/Issue.java
@@ -1,6 +1,6 @@
 package dao;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
@@ -12,5 +12,74 @@ public class Issue {
     private String text;
     private LocalDateTime date;
     private Set<Programmer> programmers;
-    private
+    private Project project;
+    private Repository repository;
+
+    @Id
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(nullable = false)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(nullable = false)
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Column(nullable = false)
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @ManyToMany
+    public Set<Programmer> getProgrammers() {
+        return programmers;
+    }
+
+    public void setProgrammers(Set<Programmer> programmers) {
+        this.programmers = programmers;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "id_project", referencedColumnName = "id", nullable = false)
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_repository", referencedColumnName = "id", nullable = false)
+    public Repository getRepository() {
+        return repository;
+    }
+
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
 }
