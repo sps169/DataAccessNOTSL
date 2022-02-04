955122be4ac8ac5ea13c44458ade82068cf56b95
Sergio Perez <sps169@outlook.es>
2022-02-04 05:30:22 +0100

    javadoc done

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"app"},{"l":"controller"},{"l":"dao"},{"l":"dto"},{"l":"manager"},{"l":"mapper"},{"l":"repository"},{"l":"service"}]
